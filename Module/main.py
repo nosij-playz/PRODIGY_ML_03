e4fdca3d8984ab1b8f0cc71ade071fd6660dd8e7
Jison Joseph Sebastian <jisonjosephsebastian7007@gmail.com>
2025-05-21 21:38:39 +0530

    Update main.py

--- Module/main.py
+++ Module/main.py
@@ -1,4 +1,8 @@
-from catdogclassify import CatDogPredictor
-predictor = CatDogPredictor()
-img_path = "/content/downloaded_image.jpg"
-print("Prediction:", predictor.predict(img_path))
+from catdogclassifier import SVMPredictor
+image_path = "/content/downloaded_image.jpg"
+if not os.path.exists(image_path):
+  print(f"Image not found: {image_path}")
+else:
+  predictor = SVMPredictor()
+  label = predictor.predict(image_path)
+  print(f"\n🖼️ Predicted Label: {label}")
